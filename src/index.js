232f15c9f2478dfe5d867101b21010f6bb5efb83
portfolio <errorcoderecords@gmail.com>
2024-12-24 13:47:06 +0100

    updt

--- src/index.js
+++ src/index.js
@@ -7,5 +7,44 @@ window.openModal = function () {
   };
 
   window.closeModal = function () {
-    document.getElementById("modal").style.top = "-650px";
+    document.getElementById("modal").style.top = "-900px";
   };
+
+  // Smooth Scroll
+      
+const links = document.querySelectorAll('.header__nav-link');
+
+links.forEach(link => {
+  link.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const targetId = this.getAttribute('href');
+    const targetElement = document.querySelector(targetId);
+
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop, 
+        behavior: 'smooth' 
+      });
+    }
+  });
+});
+
+document.querySelectorAll('.footer__footer-link').forEach(link => {
+  link.addEventListener('click', function(event) {
+    event.preventDefault(); 
+    
+    const targetId = this.getAttribute('href'); 
+    const targetElement = document.querySelector(targetId);
+    
+    if (targetElement) {
+      
+      targetElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    }
+  });
+});
+
+
